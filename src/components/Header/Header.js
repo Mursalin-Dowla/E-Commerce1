f6b540c77d5d92bc1c4bd8fefdef619d8e28368d
Mursalin-Dowla <justjim844@gmail.com>
2023-01-16 04:32:29 +0600

    css improved

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../images/Logo.svg';
+import logo2 from '../../images/logo.jpg';
 import {Link} from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
-           <img src={logo} alt="" />
+           <img src={logo2} alt="" />
            <div>
              <Link to="/shop">Shop</Link>
              <Link to="/orders">Orders</Link>
